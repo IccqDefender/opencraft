074f6c5b8be8f7179cf419d70f76f275b28fefd1
sterben <mickromacsis29v2.0@gmail.com>
2026-06-25 23:10:28 +0700

    first commit

---
+++ build/CMakeFiles/opencraft.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencraft.dir/link.d"
+  "CMakeFiles/opencraft.dir/src/main.cpp.o"
+  "CMakeFiles/opencraft.dir/src/main.cpp.o.d"
+  "opencraft"
+  "opencraft.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencraft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "GLFW/glfw3.h"
+
+int main(){
+
+  if (!glfwInit()){
+    std::cerr << "Failed to init GLFW\n";
+    return -1;
+  }
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window = glfwCreateWindow(800, 600, "opencraft", nullptr, nullptr);
+  if (!window) {
+    std::cerr << "Failed to create window!\n";
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwShowWindow(window);
+
+  while (!glfwWindowShouldClose(window)){
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
